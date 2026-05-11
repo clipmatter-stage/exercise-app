b399fea54d0ee58b032a9b63482e0c60a41a8c58
ghazi <clipmatter@gmail.com>
2026-05-11 20:11:20 +0500

    Add Carbon import to ExerciseController for date handling

--- app/Http/Controllers/ExerciseController.php
+++ app/Http/Controllers/ExerciseController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Carbon;
 
 
 class ExerciseController extends Controller
